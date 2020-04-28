ad1408c0fbc63a0701ede4cb179836f5a45a7f39
khouyaoui <ahrir@live.com>
2020-04-28 02:46:54 +0200

    practice atr android:onClick | findViewById

---
+++ Mislugares/app/src/main/java/com/moha/mislugares/AcercaDeActivity.java
@@ -0,0 +1,12 @@
+package com.moha.mislugares;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcercaDeActivity extends Activity {
+    @Override public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.acercade);
+    }
+
+}
--- Mislugares/app/src/main/java/com/moha/mislugares/MainActivity.java
+++ Mislugares/app/src/main/java/com/moha/mislugares/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moha.mislugares;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,15 +12,23 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+private Button bAcercaDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bAcercaDe = findViewById(R.id.B3_acerca);
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                lanzarAcercaDe(null);
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button bSalie = findViewById(R.id.B4_salir);
+        bSalie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
@@ -52,5 +69,10 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void lanzarAcercaDe(View view){
+        Intent intent = new Intent(this, AcercaDeActivity.class);
+        startActivity(intent);
     }
 }
